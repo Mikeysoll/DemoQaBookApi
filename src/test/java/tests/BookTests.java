81aef3cb56caa6d4cc85c7609de29b7974ba3534
Mikhail Solovyov <solovev02@yandex.ru>
2025-12-09 13:44:24 +0300

    added steps

--- src/test/java/tests/BookTests.java
+++ src/test/java/tests/BookTests.java
@@ -1,40 +1,64 @@
 package tests;
-import org.openqa.selenium.Cookie;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Cookie;
 import tests.api.AuthorizationAPI;
 import tests.api.BasketAPI;
 import tests.base.TestBase;
 import tests.models.AddBookResponse;
 import tests.models.AuthResponse;
 
-import static com.codeborne.selenide.Condition.*;
-
-
+import static com.codeborne.selenide.Condition.exist;
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.WebDriverRunner.getWebDriver;
 import static tests.data.TestData.*;
 
-
 public class BookTests extends TestBase {
-    @Test
-    public void deleteBookTest(){
-        AuthorizationAPI authorization = new AuthorizationAPI();
-        AuthResponse authResponse = authorization.login(login, password);
 
+    @Test
+    public void deleteBookTest() {
+        AuthResponse authResponse = loginViaAPI();
+        clearAndAddBook(authResponse);
+        openProfileAndSetCookies(authResponse);
+        deleteBook();
+        verifyBookDeleted();
+    }
+
+    @Step("Вход через API и получение токена")
+    private AuthResponse loginViaAPI() {
+        AuthorizationAPI authorization = new AuthorizationAPI();
+        return authorization.login(login, password);
+    }
+
+    @Step("Очистка корзины и добавление книги")
+    private void clearAndAddBook(AuthResponse authResponse) {
         BasketAPI basket = new BasketAPI();
         basket.clearBasket(authResponse.getUserId(), authResponse.getToken());
-        AddBookResponse add = basket.addBook(authResponse.getUserId(), authResponse.getToken(), isbn);
+        basket.addBook(authResponse.getUserId(), authResponse.getToken(), isbn);
+    }
 
-        open("/images/favicon.ico");
+    @Step("Открываем профиль и устанавливаем куки для авторизации")
+    private void openProfileAndSetCookies(AuthResponse authResponse) {
+        open("/images/favicon.ico"); // нужен любой URL, чтобы Selenide инициализировал драйвер
         getWebDriver().manage().addCookie(new Cookie("userID", authResponse.getUserId()));
         getWebDriver().manage().addCookie(new Cookie("expires", authResponse.getExpires()));
         getWebDriver().manage().addCookie(new Cookie("token", authResponse.getToken()));
-
         open("/profile");
-        $("#userName-value").shouldHave(text("Biam"));
-        $(".mr-2 a").shouldHave(attributeMatching("href", ".*" + isbn));
+    }
+
+    @Step("Удаляем книгу из профиля")
+    private void deleteBook() {
         $("#delete-record-undefined").click();
         $("#closeSmallModal-ok").click();
     }
 
+    @Step("Проверяем, что книга удалена")
+    private void verifyBookDeleted() {
+        $("#userName-value").shouldHave(Condition.text("Biam"));
+//        $(".mr-2 a").shouldNotHave(Condition.attributeMatching("href", ".*" + isbn));
+        $$(".mr-2 a").findBy(Condition.attribute("href", isbn)).shouldNot(exist);
+
+    }
 }
